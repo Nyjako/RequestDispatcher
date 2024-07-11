e1b812119e98d61e20c79d30e5dc81095b002ec7
Nyjako <kacper.tucholski.kt@gmail.com>
2024-07-11 13:55:13 +0200

    Added fetching libcurl if not found

--- cmake/libs.cmake
+++ cmake/libs.cmake
@@ -1,15 +1,26 @@
-# Stolen from https://github.com/jpbarrette/curlpp
-
 message(STATUS "Looking for CURL")
+
+
 include(FindCURL)
 find_package(CURL REQUIRED)
 
-if(CURL_FOUND)
+if(NOT CURL_FOUND)
   message(STATUS "Found CURL version: ${CURL_VERSION_STRING}")
   message(STATUS "Using CURL include dir(s): ${CURL_INCLUDE_DIRS}")
   message(STATUS "Using CURL lib(s): ${CURL_LIBRARIES}")
 else()
-  message(FATAL_ERROR "Could not find CURL")
+  message(NOTICE "Could not find CURL trying to fetch")
+  include(FetchContent)
+
+  FetchContent_Declare(curl
+    URL https://curl.se/download/curl-8.8.0.tar.gz
+    DOWNLOAD_EXTRACT_TIMESTAMP true
+    OVERRIDE_FIND_PACKAGE # this one
+  )
+
+  FetchContent_MakeAvailable(curl)
+
+  find_package(curl) # probably with CONFIG REQUIRED
 endif()
 
 # All following targets should search these directories for headers
